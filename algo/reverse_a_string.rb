51495b2a9beb0d018d51fba805551e3eed737c7c
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-07 13:35:30 -0800

    update reverse_a_string class

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -1,10 +1,18 @@
-# Complete the method called reverse_a_string that accepts a string as a parameter and 
+# Complete the method called reverse_a_string that accepts a string as a parameter and
 # returns the reverse. The one caveat: Don't use the reverse method that already
 # comes with Ruby!
 
-def reverse_a_string(string)
+  def reverse_a_string(string)
+    string_length = string.length
 
-end
+      new_string = ''
+      	while string_length > 0
+      		string_length -= 1 # arrays start with 0, so we need to do end_loop -1 first
+      		new_string += string[string_length]
+      	end
+      	new_string
+  end
+
 
 # Driver code - don't touch anything below this line.
 puts "TESTING reverse_a_string..."
